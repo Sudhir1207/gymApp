3056df3dae858d2ea56f6a5e1f1c6781b9ce5132
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2024-12-13 20:22:37 +0530

    completed index 02

--- client/src/components/Generator.jsx
+++ client/src/components/Generator.jsx
@@ -21,7 +21,7 @@ const Generator = () => {
   const [showModal, setShowModal] = useState(false);
   const [poison, setPoison] = useState("individual");
   const [muscles, setMuscles] = useState([]);
-  const [goals, setGoals] = useState("strength_power");
+  const [goal, setGoal] = useState("strength_power");
   function toggleModal() {
     setShowModal(!showModal);
   }
@@ -41,7 +41,7 @@ const Generator = () => {
             return (
               <button
                 onClick={() => {
-                  setPoison;
+                  setPoison(type);
                 }}
                 className={
                   "bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg" +
@@ -67,7 +67,31 @@ const Generator = () => {
             <p>Select muscle groups </p>
             <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
           </button>
-          {showModal && <div>modal</div>}
+          {showModal && (
+            <div className="flex flex-col p-3">
+              {(poison === "individual"
+                ? WORKOUTS[poison]
+                : Object.keys(WORKOUTS[poison])
+              ).map((muscleGroup, muscleGroupIndex) => {
+                return (
+                  <button
+                    onClick={() => {
+                      updateMuscles(muscleGroup);
+                    }}
+                    key={muscleGroupIndex}
+                    className={
+                      "hover:text-blue-400 duration-200 " +
+                      (muscles.includes(muscleGroup) ? " text-blue-400" : " ")
+                    }
+                  >
+                    <p className="uppercase">
+                      {muscleGroup.replaceAll("_", " ")}
+                    </p>
+                  </button>
+                );
+              })}
+            </div>
+          )}
         </div>
         <Header
           index={"03"}
@@ -78,7 +102,13 @@ const Generator = () => {
           {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
             return (
               <button
-                className="bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg"
+                onClick={() => {
+                  setGoal(scheme);
+                }}
+                className={
+                  "bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg " +
+                  (scheme === goal ? "border-blue-600 " : " border-blue-400")
+                }
                 key={schemeIndex}
               >
                 <p className="capitalize">{scheme.replaceAll("_", " ")}</p>
